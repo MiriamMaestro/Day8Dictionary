3a6e2a769e5af053ee9623a30c79fb8212c7c6d2
Miriam Maestro <miriam.maestro@zeninternet.co.uk>
2021-12-14 09:22:52 +0000

    Add project files.

---
+++ Day8Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day8Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = Convert.ToInt32(Console.ReadLine());
+
+            Dictionary<string, string> bookDirectory = new Dictionary<string, string>();
+
+            for (int i = 0; i < time; i++)
+            {
+                string[] temp = Console.ReadLine().Split(' ');
+                bookDirectory.Add(temp[0], temp[1]);
+
+            }
+
+            for (int i = 0; i < time; i++)
+            {
+
+                var s = Console.ReadLine();
+                if (bookDirectory.ContainsKey(s))
+                {
+                    Console.WriteLine(s + "=" + bookDirectory[s]);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+
+        }
+    }
+}
